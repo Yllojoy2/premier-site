bd6e8462c842477db26f4d4f12922a674aa22d5f
Yllojoy2 <116189660+Yllojoy2@users.noreply.github.com>
2023-01-25 18:08:30 +0300

    1 binaries uploaded

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,25 @@
 <script>
-	import Counter from './Counter.svelte'
-	import welcome from '$lib/images/svelte-welcome.webp'
-	import welcome_fallback from '$lib/images/svelte-welcome.png'
 	import logo from '$lib/images/logo.png'
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
 
-		to your new<br />Voltaire Club Info app !
-	</h1>
-	<img alt="logo club informatique" width="300px" src={logo} />
+	<h1>Salut les geeks! :P</h1>
+	<img alt="logo club informatique" width="200px" src={logo} />
 
 	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
+		Bienvenue au Club info du lycée Voltaire! :D
 	</h2>
 
-	<Counter />
+<ul>
+<li>Yllojoy2</li>
+<li>ZeMadCoder</li>
+<li>Feep0</li>
+<li>AliZarrade</li>
+<li>Ninoou</li>
+<li>Azert9000</li>
+<li>Majdx7</li>
+</ul>
 </section>
 
 <style>
@@ -41,21 +33,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
